a09b99eaa064f7fe6d1b83f1f13332df28ab7502
vsmm-world <thekingjarvis792@gmail.com>
2024-08-10 18:09:59 +0530

    Add cookie-parser middleware for handling cookies in authentication routes

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const { connectToMongo } = require('./db/mongo.connection');
 const port = 3000 || process.env.PORT;
+const authRouter = require('./routers/auth.router');
 
 connectToMongo();
+
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use("/api/auth", authRouter);
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     console.log('Request', req.header);
--- routers/auth.router.js
+++ routers/auth.router.js
@@ -2,6 +2,15 @@ const express = require('express');
 const { authenticateToken } = require('../shared/auth/jwt.authentication');
 const router = express.Router();
 
+
+router.post('/register', (req, res) => {
+    res.send('Register route');
+});
+
+router.post('/login', (req, res) => {
+    res.send('Login route');
+});
+
 router.get('/', authenticateToken, (req, res) => {
     const user = req.user;
     res.send(`Welcome to the authentication router, ${user.name}`);
@@ -9,3 +18,4 @@ router.get('/', authenticateToken, (req, res) => {
 });
 
 module.exports = router;
+
