9ecc34903203338f90bfa6fad57d9fb6d4068b4c
Ravindra Valand <ravindra@linearloop.io>
2024-08-12 12:54:12 +0530

    enable cors for https

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
@@ -10,20 +11,21 @@ const authRouter = require('./routers/auth.router');
 
 connectToMongo();
 
+// Enable CORS for all incoming requests
+app.use(cors());
+
 // Ensure express.json() middleware is used before any router to parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Changed extended to true to support parsing of arrays and objects
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
 
-app.post('/', (req, res) => {
-    console.log('Request Body:', req.body);
-    res.send('Hello World');
-});
+
 
 app.get('/', (req, res) => {
-    console.log('Request Headers:', req.headers);
-    res.send('Hello World');
+    res.status(200).send({
+        message: 'Express js Backend API Starting Template Developed by Ravindra Valand'
+    });
 });
 
 app.listen(port, () => {
