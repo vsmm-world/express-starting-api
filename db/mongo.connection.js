fe66a059e9fd8f44ec73dd3433d8e1d0f5a670fd
vsmm-world <thekingjarvis792@gmail.com>
2024-10-19 11:53:20 +0530

    optimize mongodb connection

--- db/mongo.connection.js
+++ db/mongo.connection.js
@@ -6,11 +6,19 @@ const uri = process.env.MONGO_DB_URI;
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(uri);
+        await mongoose.connect(uri, {
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+        });
         console.log('Connected to MongoDB');
     } catch (error) {
-        console.error('Error connecting to MongoDB', error);
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
     }
 }
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('MongoDB connection is open'));
 
 module.exports = { connectToMongo };
